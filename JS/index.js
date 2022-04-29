995a3143bbb1aca79fd47d73604e9fe442ace598
MatiBravo <Matibravoneron@gmail.com>
2022-04-29 11:34:37 -0300

    Hice 8 ejercicios de practica de JS

---
+++ JS/index.js
@@ -0,0 +1,64 @@
+//1-Escribe un programa de una sola línea que haga que aparezca en la pantalla un
+//alert que diga “Hello World”.
+
+//alert("Hello World")
+
+//2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
+//diga “Hello World” (document.write).
+
+//document.write("Hello World")
+
+//3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
+//de sumar 3 + 5.
+
+//document.write(3+5)
+
+//4. Escribe un programa de dos líneas que pida el nombre del usuario con un
+//prompt y escriba un texto que diga “Hola nombreUsuario”
+
+//var nombre = prompt ("Ingrese su nombre", "")
+//document.write("Hola " + nombre)
+
+//5. Escribe un programa de tres líneas que pida un número, pida otro número y
+//escriba el resultado de sumar estos dos números.
+
+//var num1 = prompt ('Ingrese un numero');
+//var num2 = prompt ("Ingrese otro numero ");
+//document.write("la suma es: " + (parseInt(num1)+parseInt(num2)))
+
+//6. Escribe un programa que pida dos números y escriba en la pantalla cual es el
+//mayor
+
+//var num1 = prompt ('Ingrese un numero');
+//var num2 = prompt ("Ingrese otro numero ");
+//if (num1>num2) {
+//    document.write(num1)
+//}else{
+// document.write ("el mayor es: " + num2) }
+
+//7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de
+//los tres                                                                                     NO ME SALIO!!!
+
+//var n1 = prompt("Escribe un número");
+//var n2 = prompt("Escribe otro número");
+//var n3 = prompt("Escribe otro número");
+//if (n1 > n2 && n1 > n3) {
+//document.write(n1);
+//} else if (n2 > n1 && n2 > n3) {
+//document.write(n2);
+//} else if (n3 > n1 && n3 > n2){
+//document.write(n3);
+//}
+
+//8. Escribe un programa que pida un número y diga si es divisible por 2
+
+//var n1 = prompt("Ingrese un numero");
+//if (n1%2===0) {
+//    document.write("El numero "+ n1 + " es divisible ")
+//} else {
+//    document.write("El numero "+n1 + " no es divisible")
+//}
+//9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
+//letra a
+
+
