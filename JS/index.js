413f9e40677f069779a3742b73fbe0b1b912fc84
MatiBravo <Matibravoneron@gmail.com>
2022-05-05 21:29:19 -0300

    Hice otro ejercicio

--- JS/index.js
+++ JS/index.js
@@ -60,3 +60,19 @@
 //}
 //9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
 //letra a
+
+//10. Escribe un programa que pida una frase y escriba las vocales que aparecen
+// var text = prompt("Ingrese una frase ");
+// var nText = text.length;
+// var i;
+// for (i = 0; i < nText; i++ ) {
+// if (text.substr(i,1) === "a" || text.substr(i,1) === "e" ||
+// text.substr(i,1) === "i" || text.substr(i,1) === "o" 
+// || text.substr(i,1) === "u" ){
+// document.write (text.substring(i,1));
+// }
+// }
+// 11. Escribe un programa que pida una frase y escriba cuántas de las letras que
+// tiene son vocales
+// 12. Escribe un programa que pida una frase y escriba cuántas veces aparecen
+// cada una de las vocales
