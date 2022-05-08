97bf729b11fcc9d83dc152ec4847d5948d611eee
MatiBravo <Matibravoneron@gmail.com>
2022-05-07 21:32:00 -0300

    Ejercicio 19 hecho

--- JS/index.js
+++ JS/index.js
@@ -154,5 +154,31 @@
 //6-7: Bien
 //7-9: Notable
 //9-10: Sobresaliente
-//var nota= prompt("Ingrese la nota")
+var nota= prompt("Ingrese la nota");
+//Si es un numero
+if (Number(nota)==nota){
+  if(nota > 0 && nota <=10 ){
+      if (nota < 3) {
+          alert("Muy deficiente");
+      }
+      else if (nota < 5) {
+          alert("Insuficiente")
+      }
+      else if (nota <= 6){
+          alert("Suficiente")
+      }
+      else if (nota <= 7){
+          alert("Bien")
+        }
+      else if (nota <= 9){
+          alert("Notable")
+      }
+      else if (nota >=10){
+          alert("Sobresaliente")
+      }
+}
+else 
+ alert("Nota erronea")
+}
+
 
