abebb0e6e5feeaeec1ed47f02d42180be4fcb884
MatiBravo <Matibravoneron@gmail.com>
2022-05-07 21:45:12 -0300

    Ejercicio 20

--- JS/index.js
+++ JS/index.js
@@ -154,31 +154,44 @@
 //6-7: Bien
 //7-9: Notable
 //9-10: Sobresaliente
-var nota= prompt("Ingrese la nota");
-//Si es un numero
-if (Number(nota)==nota){
-  if(nota > 0 && nota <=10 ){
-      if (nota < 3) {
-          alert("Muy deficiente");
-      }
-      else if (nota < 5) {
-          alert("Insuficiente")
-      }
-      else if (nota <= 6){
-          alert("Suficiente")
-      }
-      else if (nota <= 7){
-          alert("Bien")
-        }
-      else if (nota <= 9){
-          alert("Notable")
-      }
-      else if (nota >=10){
-          alert("Sobresaliente")
-      }
-}
-else 
- alert("Nota erronea")
-}
+// var nota= prompt("Ingrese la nota");
+// //Si es un numero
+// if (Number(nota)==nota){
+//   if(nota > 0 && nota <=10 ){
+//       if (nota < 3) {
+//           alert("Muy deficiente");
+//       }
+//       else if (nota < 5) {
+//           alert("Insuficiente")
+//       }
+//       else if (nota <= 6){
+//           alert("Suficiente")
+//       }
+//       else if (nota <= 7){
+//           alert("Bien")
+//         }
+//       else if (nota <= 9){
+//           alert("Notable")
+//       }
+//       else if (nota >=10){
+//           alert("Sobresaliente")
+//       }
+// }
+// else 
+//  alert("Nota erronea")
+// }
+
+//20.Realiza un script que pida cadenas de texto hasta que se pulse "cancelar"
+//Al salir con "cancelar" deben mostrarse todas las cadenas concatenadas con un guion
+// var resultado =""
+// do{
+// var cadena= prompt("Ingrese cadena de texto ");
+// if (resultado ==""){
+//     resultado = resultado + cadena;
+// }else{
+//     resultado = resultado+ "-"+ cadena;
+// }
+// }while (confirm("Desea continuar"));
+// document.write(resultado);
 
 
