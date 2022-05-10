bf329ba63ad275da20f83b89567e4b1e6f930664
MatiBravo <Matibravoneron@gmail.com>
2022-05-10 18:46:26 -0300

    Agrego archivo jsv unidad 2

---
+++ JS/Unidad2.js
@@ -0,0 +1,13 @@
+// UNIDAD 2
+// Tema: Funciones 
+// 1.Realizar una funcion,a la que se le pase como parametro un numero N,
+// y muestre por consola N veces,el siguiente mensaje: "Bienvenidos al curso de Full Stack"
+// function repetirTexto(n){
+//     for (let i=0; i< n;i++)
+//          console.log("Bienvenidos al curso full Stack")
+// }
+// //Programa principal 
+// let n=parseInt(prompt("Ingrese un numero entero:"))
+// repetirTexto(n)
+
+//2.Diseniar una funcion que tenga como parametros dos numeros,y que calcule el maximo
--- JS/index.js
+++ JS/index.js
@@ -215,14 +215,4 @@
 //23
 
 
-// UNIDAD 2
-// Tema: Funciones 
-// 1.Realizar una funcion,a la que se le pase como parametro un numero N,
-// y muestre por consola N veces,el siguiente mensaje: "Bienvenidos al curso de Full Stack"
-function repetirTexto(n){
-    for (let i=0; i< n;i++)
-         console.log("Bienvenidos al curso full Stack")
-}
-//Programa principal 
-let n=parseInt(prompt("Ingrese un numero entero:"))
-repetirTexto(n)
+
