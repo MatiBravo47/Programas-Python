9a67b797bcfa5f7e33cbfedc4e47bddf8346f907
MatiBravo <Matibravoneron@gmail.com>
2022-05-10 18:38:38 -0300

    Ejercicio 1

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+// UNIDAD 1 
+
 //1-Escribe un programa de una sola línea que haga que aparezca en la pantalla un
 //alert que diga “Hello World”.
 //alert("Hello World")
@@ -213,3 +215,14 @@
 //23
 
 
+// UNIDAD 2
+// Tema: Funciones 
+// 1.Realizar una funcion,a la que se le pase como parametro un numero N,
+// y muestre por consola N veces,el siguiente mensaje: "Bienvenidos al curso de Full Stack"
+function repetirTexto(n){
+    for (let i=0; i< n;i++)
+         console.log("Bienvenidos al curso full Stack")
+}
+//Programa principal 
+let n=parseInt(prompt("Ingrese un numero entero:"))
+repetirTexto(n)
