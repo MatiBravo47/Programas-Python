74edeea743f357316f40dc11fc9a95b79f283c2e
Your Name <Matibravoneron@gmail.com>
2022-05-30 17:39:53 -0300

    Agregos ejercicios

---
+++ Ejercicios.py
@@ -0,0 +1,9 @@
+#Escribe un programa muestre por pantalla "Hello World"
+#print("Hello World")
+
+#Escribe un programa que escriba en la pantalla el resultado
+#de sumar 3 + 5
+#print(3+5)
+
+#Escribe un programa que pida el nombre del usuario y 
+#escriba un texto que diga “Hola nombreUsuario”
