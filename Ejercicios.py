91ffde08d3f56578204480b183edc84e66a85303
Matias Bravo Neron <99216042+MatiBravo47@users.noreply.github.com>
2025-02-19 20:41:12 -0300

    Update Ejercicios.py
    Elimino comentarios

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,44 +1,44 @@
 #Actividad practica-Python UNIDAD 1
 #Escribe un programa muestre por pantalla "Hello World"
-#print("Hello World")
+print("Hello World")
 
 #Escribe un programa que escriba en la pantalla el resultado
 #de sumar 3 + 5
-#print(3+5)
+print(3+5)
 
 #Escribe un programa que pida el nombre del usuario y 
 #escriba un texto que diga “Hola nombreUsuario”
-#nombreUsuario=input("Ingrese su nombre")
-#print("Hola "+nombreUsuario)
+nombreUsuario=input("Ingrese su nombre")
+print("Hola "+nombreUsuario)
 
 #4) Escribe un programa que pida un número, pida otro número 
 #y escriba el resultado de sumar estos dos números.
-#A=int(input("Ingrese un numero"))
-#B=int(input("Ingrese otro numero"))
-#print(A + B)
+A=int(input("Ingrese un numero"))
+B=int(input("Ingrese otro numero"))
+print(A + B)
 
 #5)Escribe un programa que pida dos numeros y escriba 
 #en la pantalla cual es el mayor .
-#a=int(input("Ingrese un numero"))
-#b=int(input("Ingrese otro numero"))
-#if a > b: 
-#    print("a es mayor que b" )
-#else : 
-#    print("b es mayor que a")
+a=int(input("Ingrese un numero"))
+b=int(input("Ingrese otro numero"))
+if a > b: 
+    print("a es mayor que b" )
+else : 
+    print("b es mayor que a")
 
 #7)Escribe un programa que pida un numero y nos diga si
 # es divisible por 2
-#a=int(input("Ingrese un numero "))
-#if a % 2 == 0 :
-#    print("Es divisible por 2")
-#else:
-#    print("No es divisible por 2")
+a=int(input("Ingrese un numero "))
+if a % 2 == 0 :
+    print("Es divisible por 2")
+else:
+    print("No es divisible por 2")
 
 #10)Escribir un programa que escriba en pantalla los 
 #divisores de un numero dado.
 
-#a=int(input("Ingrese un numero"))
-#for () 
+a=int(input("Ingrese un numero"))
+for () 
 
 try:
     print(1)
